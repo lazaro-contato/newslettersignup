dbe8cda11efe5d861f90c11745211f90a971805e
jlazaro <you@example.com>
2021-06-07 22:36:11 -0300

    adding the css to the file

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-// ----------------- REQUIRES ----------------- //
-const express = require("express");
-const bodyParser = require("body-parser");
-const request = require("request")
-
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+ 
 const app = express();
-app.use(bodyParser.urlencoded({extended:true}))
-
-// ----------------- GET METHOD ----------------- //
-app.get("/", function(req, res){
-    res.sendFile(__dirname + "/signup.html")
-})
+app.use(bodyParser.urlencoded({ extended: true}));
  
 
-// ----------------- LISTENER ----------------- //
-app.listen(3000, function(){
-    console.log("Server rodando")
-})
+ 
+app.use(express.static('public'));
+app.use(express.static(__dirname));
+ 
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/signup.html');
+});
+ 
+app.listen(3000, function() {
+     console.log("Example app listening MACHO");
+});
